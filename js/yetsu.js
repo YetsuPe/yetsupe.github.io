87de7848f2aec4b96e2b264c2bd156ea6e847511
skeiter9 <skeiter97@gmail.com>
2013-07-13 16:44:56 -0500

    UI colors

--- js/yetsu.js
+++ js/yetsu.js
@@ -2,8 +2,7 @@
 /*Zepto js*/
 Zepto(function($){
   	$('#ico-menu').on('click',function(){
-  		alert(window.innerWidth+" | viewport "+$(window).width()+" | width html document "+$(document).width() );
-
+  		//alert(window.innerWidth+" | viewport "+$(window).width()+" | width html document "+$(document).width() );
   		$('body').css({'overflow-x':'hidden'});
   		$('header,section,footer').toggleClass('move-blocks');
 		$('#menu').toggleClass('open-menu');
