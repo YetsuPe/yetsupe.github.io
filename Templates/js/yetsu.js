3eea6c507db8eedd1da1b4f9c884fff35551d5e9
skeiter9 <skeiter97@gmail.com>
2013-08-08 00:49:19 -0500

    cources section

--- Templates/js/yetsu.js
+++ Templates/js/yetsu.js
@@ -4,11 +4,14 @@ Zepto(function($){
 	var menu = new yetMenu_view(); //load yetMenu
 	var user = new yetUser_view(); //load yetMenu
 	new yetSocial_view(); //load yetLocal map
+
+
+	/********* Touch events **************/
 	$$('body').swipeRight( function(){
 		menu.touchOpenMenu();
 	});
 	$$('body').swipeLeft( function(){
 		menu.touchCloseMenu();
 	});
-
+	/******* ./Touch events ***************/
 });
