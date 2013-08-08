86d631ce2ff30540ff33c32a0042335f99b0d455
skeiter9 <skeiter97@gmail.com>
2013-08-08 16:57:52 -0500

    mobile version beta

--- Templates/js/yetsu.js
+++ Templates/js/yetsu.js
@@ -8,18 +8,18 @@ Zepto(function($){
 
 
 	/********* Touch events **************/
-	/*$$('body').swipeRight( function(){
+	$$('body').swipeRight( function(){
 		menu.touchOpenMenu();
 	});
 	$$('body').swipeLeft( function(){
 		menu.touchCloseMenu();
-	});*/
+	});/*
 	$$('#slide-pictures').swipeLeft( function(){
 		slider.slideLeft();
 	});
 	$$('#slide-pictures').swipeRight( function(){
 		slider.slideRight();
-	});
+	});*/
 
 	/******* ./Touch events ***************/
 });
