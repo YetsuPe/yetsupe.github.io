c3c27ad4ff71930fa6ca5eb2c5608a8af435e24f
skeiter9 <skeiter97@gmail.com>
2013-08-07 22:17:06 -0500

    Event touch -> show menu swipeRight

--- Templates/js/yetsu.js
+++ Templates/js/yetsu.js
@@ -4,4 +4,10 @@ Zepto(function($){
 	new yetMenu_view(); //load yetMenu
 	new yetUser_view(); //load yetMenu
 	new yetSocial_view(); //load yetLocal map
+	$('body').swipeRight( function(){
+		console.log("swipeRight ");
+		$('body').css({'overflow-x':'hidden'});
+  	$('header,section,footer,aside').toggleClass('move-blocks');
+		$('#menu').toggleClass('open-menu');
+	});
 });
