c81e51fc1dbf009f8826a252b168748456d84f30
skeiter9 <skeiter97@gmail.com>
2013-10-04 22:45:21 -0500

    New version, now with Less and start use bootstrap grid

--- Templates/js/yetsu-old.js
+++
@@ -1,8 +0,0 @@
-/*jquery*/
-$(document).on("ready",function(){
-	new yetMap_view(); //load yetLocal map
-	new yetMenu_view(); //load yetMenu
-	new yetSocial_view(); //load yetLocal map
-	//alert(document.documentElement.clientWidth+" | viewport "+$(window).width()+" | width html document "+$(document).width() );
-
-});
