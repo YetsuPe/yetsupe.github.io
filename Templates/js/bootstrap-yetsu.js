a4e974b1941f3c940f685070897aedad81baa12f
skeiter9 <skeiter97@gmail.com>
2013-10-07 15:28:53 -0500

    New section markup

--- Templates/js/bootstrap-yetsu.js
+++ Templates/js/bootstrap-yetsu.js
@@ -284,80 +284,95 @@
 
 $(document).on("ready",function(){
 
+    
+
+  google.maps.event.addDomListener(window,'load',function(){
+
+
     var styles = [
-    {
-      featureType: "landscape.man_made",
-      elementType: "geometry",
-      stylers: [
-        { color: "#e67e22" }
-      ]
-    },{
-      featureType: "landscape.natural.landcover",
-      elementType: "geometry",
-      stylers: [
-        { color: "#ff0000" }
-      ]
-    },{
-      featureType: "poi.business",
-      elementType: "geometry",
-      stylers: [
-        { color: "#66CBFF" }
-      ]
-    },{
-      featureType: "poi.attraction",
-      elementType: "geometry",
-      stylers: [
-        { color: "#fff55b" }
-      ]
-    },{
-      featureType: "poi.park",
-      elementType: "geometry",
-      stylers: [
-        { color: "#1abc9c" }
-      ]
-    },{
-      featureType: "road",
-      elementType: "geometry",
-      stylers: [
-        { color: "#7f8c8d" }
-      ]
-    },{
-      featureType: "road",
-      elementType: "labels.text.fill",
-      stylers: [
-        { color: "#ffffff" }
-      ]
-    },{
-      featureType: "road",
-      elementType: "labels.text.stroke",
-      stylers: [
-        { color: "7f8c8d" }
-      ]
-    }
+      {
+        featureType: "landscape.man_made",
+        elementType: "geometry",
+        stylers: [
+          { color: "#e67e22" }
+        ]
+      },{
+        featureType: "landscape.natural.landcover",
+        elementType: "geometry",
+        stylers: [
+          { color: "#ff0000" }
+        ]
+      },{
+        featureType: "poi.business",
+        elementType: "geometry",
+        stylers: [
+          { color: "#66CBFF" }
+        ]
+      },{
+        featureType: "poi.attraction",
+        elementType: "geometry",
+        stylers: [
+          { color: "#fff55b" }
+        ]
+      },{
+        featureType: "poi.park",
+        elementType: "geometry",
+        stylers: [
+          { color: "#1abc9c" }
+        ]
+      },{
+        featureType: "road",
+        elementType: "geometry",
+        stylers: [
+          { color: "#7f8c8d" }
+        ]
+      },{
+        featureType: "road",
+        elementType: "labels.text.fill",
+        stylers: [
+          { color: "#ffffff" }
+        ]
+      },{
+        featureType: "road",
+        elementType: "labels.text.stroke",
+        stylers: [
+          { color: "7f8c8d" }
+        ]
+      }
 
-  ];
-  var styledMap = new google.maps.StyledMapType(styles,{name: "Styled Map"});
+    ];
+    var styledMap = new google.maps.StyledMapType(styles,{name: "Styled Map"});
 
-  var mapOptions = {
-    zoom: 18,
-    center: new google.maps.LatLng(-6.704695,-79.901065),
-    mapTypeControlOptions: {
-      mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
-    }
-  };
-  var map = new google.maps.Map(document.getElementById('yetLocal'), mapOptions);
-  map.mapTypes.set('map_style', styledMap);
-  map.setMapTypeId('map_style');
+    var mapOptions = {
+      zoom: 18,
+      center: new google.maps.LatLng(-6.704695,-79.901065),
+      mapTypeControlOptions: {
+        mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
+      }
+    };
+    var map = new google.maps.Map(document.getElementById('yetLocal'), mapOptions);
+    map.mapTypes.set('map_style', styledMap);
+    map.setMapTypeId('map_style');
 
-   var image = 'images/icons/yetsu-mark-map.png';
-   var myLatLng = new google.maps.LatLng(-6.704695,-79.901065);
-    var beachMarker = new google.maps.Marker({
-        position: myLatLng,
-        map: map,
-        icon: image
+    var infoWindow = new google.maps.InfoWindow({
+      content:"<a href='https://foursquare.com/YetsuPe'><span class='icon-home icon-2x'></span></a><p>YetsuPe</p>"
+    });
+
+     var image = 'images/icons/yetsu-mark-map.png';
+     var myLatLng = new google.maps.LatLng(-6.704695,-79.901065);
+      var beachMarker = new google.maps.Marker({
+          position: myLatLng,
+          map: map,
+          icon: image
+      });
+
+    google.maps.event.addListener( beachMarker,'click',function(){
+      infoWindow.open(map,beachMarker);
     });
 
 
+
+  });
 
     /********** map Ficsa *********/
     google.maps.event.addDomListener(window,'load',function(){
